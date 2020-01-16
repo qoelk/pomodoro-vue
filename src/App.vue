1f012611481ca8302fa1392862ce85639d33bca2
knilxar <admin@bigbrodesign.com>
2020-01-16 14:59:06 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+
+  </div>
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+
+#app {
+  background-color: black;
+  width: 100%;
+  height: 100vh;
+
+  font-family: monospace;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: white;
+
+}
+</style>
